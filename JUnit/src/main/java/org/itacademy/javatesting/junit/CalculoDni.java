3afd5296e9b74c2287c5542127044d966cc3f330
Daniel <danisolo91@gmail.com>
2021-10-11 20:52:33 +0200

    Improve getLetra()

--- JUnit/src/main/java/org/itacademy/javatesting/junit/CalculoDni.java
+++ JUnit/src/main/java/org/itacademy/javatesting/junit/CalculoDni.java
@@ -2,10 +2,10 @@ package org.itacademy.javatesting.junit;
 
 public class CalculoDni {
 
-	public static String getLetra(int numero) {
+	public static String getLetra(String n) {
+		int numero = Integer.parseInt(n);
 		String[] letras = {"T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E"};
 
 		return letras[numero % 23];
 	}
-	
 }
--- JUnit/src/test/java/org/itacademy/javatesting/junit/CalculoDniTest.java
+++ JUnit/src/test/java/org/itacademy/javatesting/junit/CalculoDniTest.java
@@ -16,24 +16,24 @@ public class CalculoDniTest {
 	@Parameters
 	public static Collection<Object[]> data() {
 		return Arrays.asList(new Object[][] { 
-			{ 12345678, "Z" },
-			{ 87654321, "X" },
-			{ 12312312, "K" },
-			{ 45645645, "Y" },
-			{ 14715912, "Y" },
-			{ 55544477, "Y" },
-			{ 65165114, "L" },
-			{ 98498445, "W" },
-			{ 43243242, "E" },
-			{ 66699987, "X" } 
+			{ "00045678", "T" },
+			{ "87654321", "X" },
+			{ "12312312", "K" },
+			{ "45645645", "Y" },
+			{ "14715912", "Y" },
+			{ "55544477", "Y" },
+			{ "65165114", "L" },
+			{ "98498445", "W" },
+			{ "43243242", "E" },
+			{ "66699987", "X" } 
 		});
 	}
 	
-	private int fInput;
+	private String fInput;
 
     private String fExpected;
 
-    public CalculoDniTest(int input, String expected) {
+    public CalculoDniTest(String input, String expected) {
         this.fInput = input;
         this.fExpected = expected;
     }
