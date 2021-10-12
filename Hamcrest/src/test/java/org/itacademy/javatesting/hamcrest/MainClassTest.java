aa4256de5d3c34432c1da70e5df7196681622521
Daniel <danisolo91@gmail.com>
2021-10-12 11:56:15 +0200

    Hamcrest custom matcher

--- Hamcrest/src/test/java/org/itacademy/javatesting/hamcrest/MainClassTest.java
+++ Hamcrest/src/test/java/org/itacademy/javatesting/hamcrest/MainClassTest.java
@@ -1,4 +1,34 @@
 package org.itacademy.javatesting.hamcrest;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.hamcrest.FeatureMatcher;
+import org.hamcrest.Matcher;
+import org.junit.Test;
+
 public class MainClassTest {
+	
+	@Test
+	public void mordorLengthIsNot8() {
+		assertThat("Mordor", length(not(8)));
+	}
+	
+	@Test
+	public void mordorLengthIs6() {
+		assertThat("Mordor", length(is(6)));
+	}
+	
+	public static  Matcher<String> length(Matcher<? super Integer> matcher) {
+	    return new FeatureMatcher<String, Integer>(matcher, "the length of a String", "length") {
+	    	
+	        @Override
+	        protected Integer featureValueOf(String actual) {
+	          return actual.length();
+	        }
+	        
+	    };
+	}
+	
 }
